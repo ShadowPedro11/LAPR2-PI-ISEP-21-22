e1123972a620af415e8fdf28fdf675dfef3d2ae9
ShadowPedro11 <68439544+ShadowPedro11@users.noreply.github.com>
2023-02-10 22:27:04 +0000

    Add files via upload

---
+++ src/main/java/app/domain/model/CenterCoordinator.java
@@ -0,0 +1,13 @@
+package app.domain.model;
+
+public class CenterCoordinator extends Employee {
+
+    public CenterCoordinator(String name, String address, int phoneNumber, String email, int citizenCardNumber){
+        super(name, address, phoneNumber, email, citizenCardNumber);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
